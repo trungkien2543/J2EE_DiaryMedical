833627a3047bd1e246db7dcb1594f2eb0d78fe04
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2024-09-25 16:11:40 +0700

    add project

---
+++ MedicalDiary/MedicalDiary/src/main/java/com/project/MedicalDiary/MedicalDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.project.MedicalDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalDiaryApplication.class, args);
+	}
+
+}
---
+++ MedicalDiary/MedicalDiary/src/test/java/com/project/MedicalDiary/MedicalDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.MedicalDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
